efeafefb544231d9e88db13c0ca82576bd012184
Own Sharayajyoti <shrayajyotidey04@gmail.com>
2026-09-12 02:28:19 +0530

    ep286_inline css

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,20 @@ let lname = "DEY";
 let luck = 7;
 const currentYear = new Date().getFullYear();
 const randomImgUrl = 'https://picsum.photos/200'; //lorem picsum
+const customCss = {
+  display: "flex",
+  color: "red",
+  backgroundColor: "black",
+  border: "3px solid red",
+};
+customCss.borderRadius = "6px";
+customCss.textAlign = "center";
+customCss.justifyContent = "center";
+// customCss.width = "90%";
+// customCss.height = "30px";
+// customCss.paddingLeft = "30px";
+// customCss.marginLeft = "30px";
+
 function App() {
   const [count, setCount] = useState(0)
   return (
@@ -26,23 +40,26 @@ function App() {
     // <p>Your Lucky Number is {luck} </p>
     // <p>Your Random Number is {Math.floor(Math.random()*10)} </p>
     // </div>
-//     <div>
-//       <footer>
-// <p>Created By {fname} {lname}.</p>
-// <p>&copy; {currentYear}.</p>
-// </footer>
-//     </div> 
+    //     <div>
+    //       <footer>
+    // <p>Created By {fname} {lname}.</p>
+    // <p>&copy; {currentYear}.</p>
+    // </footer>
+    //     </div> 
 
-<div>
-<h1 className='heading'>The Pictures</h1>
-<div className='container'>
-<img className='photo' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7zDMYnGheNT1l7pEW3R5N3Uf4--yCjRprCG9W5WQ58g&s=10' alt='Tiger Image' />
-<img className='photo' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQIAC1kBFD8UgsOayMBHWl9pktJkSO1BuY7ZpdVGmLUMg&s=10' alt='Scenary Image' />
-<img className='photo' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDSwQeYsy-xxMqXYaRUCU1BCqvSA0Y3pg20bi9UtO-Kg&s=10' alt='Human Image' />
-<img className='photo' src= {randomImgUrl} alt='Random Image' />
-<img className='photo' src= {randomImgUrl + "?grayscale"} alt='Random Gray Image' />
-</div>
-</div>
+    // <div>
+    //   <h1 className='heading'>The Pictures</h1>
+    //   <div className='container'>
+    //     <img className='photo' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7zDMYnGheNT1l7pEW3R5N3Uf4--yCjRprCG9W5WQ58g&s=10' alt='Tiger Image' />
+    //     <img className='photo' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQIAC1kBFD8UgsOayMBHWl9pktJkSO1BuY7ZpdVGmLUMg&s=10' alt='Scenary Image' />
+    //     <img className='photo' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDSwQeYsy-xxMqXYaRUCU1BCqvSA0Y3pg20bi9UtO-Kg&s=10' alt='Human Image' />
+    //     <img className='photo' src={randomImgUrl} alt='Random Image' />
+    //     <img className='photo' src={randomImgUrl + "?grayscale"} alt='Random Gray Image' />
+    //   </div>
+    // </div>
+
+    //inline css
+    <h1 style={customCss}>Hello Mr. Sdey</h1>
 
 
   )
